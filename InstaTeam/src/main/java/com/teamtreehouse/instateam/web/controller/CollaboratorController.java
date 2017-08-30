4b8a955703ba40ffd0d7e7941b543866fbac61d9
fkirwin <frederick.kirwin@gmail.com>
2017-08-29 21:12:32 -0400

    An important commit for list ordering.

--- InstaTeam/src/main/java/com/teamtreehouse/instateam/web/controller/CollaboratorController.java
+++ InstaTeam/src/main/java/com/teamtreehouse/instateam/web/controller/CollaboratorController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 import com.teamtreehouse.instateam.model.Collaborator;
 import com.teamtreehouse.instateam.model.Role;
 import com.teamtreehouse.instateam.service.CollaboratorService;
@@ -52,16 +51,49 @@ public class CollaboratorController
     }
 
     @RequestMapping(value = "/collaborators/add", method = RequestMethod.POST)
-    public String addCollaborator(@ModelAttribute("collaborator") Collaborator collaborator,  
+    public String addCollaborator(@Valid Collaborator collaborator,  
     								@RequestParam(name="collaboratorrole") String roleId,
     								BindingResult result,  
     								Model model) 
     {
     	Role role = roleService.findById(Long.parseUnsignedLong(roleId));
     	collaborator.setRole(role);
-    	
 		collaboratorService.save(collaborator);
     	return "redirect:/collaborators";
     }
-
+    
+    @RequestMapping(value = "/collaborators/{collaboratorID}/edit")
+    public String editCollaborator(@PathVariable Long collaboratorId,  Model model) 
+    {
+    	 if(!model.containsAttribute("category")) {
+             model.addAttribute("category",collaboratorService.findById(collaboratorId));
+         }
+    	
+    	return "collaborator/editcollaborator";
+    }
+    
+    @RequestMapping(value = "/collaborators/update", method = RequestMethod.POST)
+    public String updateCollaborator(@Valid Collaborator collaborator,  BindingResult result,  Model model) 
+    {
+    	
+    	
+    	
+    	return "redirect:/collaborators";
+    }
 }
+
+
+/*
+@RequestMapping(value = "/collaborators/add", method = RequestMethod.POST)
+public String addCollaborator(@ModelAttribute("collaborator") Collaborator collaborator,  
+								@RequestParam(name="collaboratorrole") String roleId,
+								BindingResult result,  
+								Model model) 
+{
+	Role role = roleService.findById(Long.parseUnsignedLong(roleId));
+	collaborator.setRole(role);
+	
+	collaboratorService.save(collaborator);
+	return "redirect:/collaborators";
+}
+*/
